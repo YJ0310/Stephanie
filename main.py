ab7313c5a93a193c69df71eb83f956ccd39aabdb
YJ0310 <89343719+YJ0310@users.noreply.github.com>
2024-11-29 00:52:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from bot import TelegramBot
 from keep_alive import keep_alive
+keep_alive()
 
 def main():
     """Initialize and start the Telegram bot."""
     bot = TelegramBot()
     bot.start()
-    keep_alive()
 
 if __name__ == '__main__':
     main()
